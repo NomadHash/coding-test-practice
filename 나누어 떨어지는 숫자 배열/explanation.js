34e623778050fa829abf47a38e95df4fbbc0a4c7
Nomad_Hash <badsoap141@gmail.com>
2020-07-08 22:26:08 +0900

    프로그래머스 코딩테스트 연습문제 '나누어 떨어지는 숫자 배열','약수의 합', '짝수와 홀수' 풀이

---
+++ 나누어 떨어지는 숫자 배열/explanation.js
@@ -0,0 +1,10 @@
+function solution(arr, divisor) {
+    let answer = [];
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] % divisor === 0 ? answer.push(arr[i]) : answer;
+    }
+    answer.length === 0 ? answer.push(-1) : answer.sort(function (a, b) {
+        return a - b;
+    });
+    return answer;
+};
