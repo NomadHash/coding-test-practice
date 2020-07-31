0740af79fad04d5ff91a1fee51d4065667ecfa29
Nomad_Hash <badsoap141@gmail.com>
2020-07-31 12:21:17 +0900

    Add 'toss test exam'

---
+++ toss/test1.js
@@ -0,0 +1,12 @@
+const ageCheck = age => {
+    const ageList = ["10","20","30","40","50","60","70","80","90"];
+    const firstChar = ageList[String(age).charAt(0) - 1];
+    let ageGroup = "";
+
+     age < 10 ? ageGroup = "애새끼 입니다." :
+     age >= 100 ? ageGroup = "100세 이상 입니다." : ageGroup = `${firstChar}대입니다.`;
+    
+     return ageGroup;
+    };
+
+console.log(ageCheck(43));
